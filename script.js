c99c6b1693ad6026a5a1ec10d240509cef0b0ee1
akiran <61043885+akiran123@users.noreply.github.com>
2024-05-07 11:36:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// タブ切り替え時の処理
 function openTab(evt, tabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -11,16 +12,20 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 
-  // Load markdown content
+  // marked.jsを読み込んだ後に処理を行う
+  loadMarkdown(tabName);
+}
+
+// Markdownファイルを読み込んでHTMLに変換し、コンテンツを表示する
+function loadMarkdown(tabName) {
   fetch(tabName + ".md")
     .then(response => response.text())
     .then(text => {
-      // Convert markdown to HTML
-      var html = marked(text);
-      // Display HTML content
+      var html = marked(text); // marked()関数を使用してMarkdownをHTMLに変換
       document.getElementById(tabName).innerHTML = html;
-    });
+    })
+    .catch(error => console.error('Error fetching markdown:', error));
 }
 
-// Open default tab on page load
+// 初期状態で最初のタブを開く
 document.getElementsByClassName("tablinks")[0].click();
